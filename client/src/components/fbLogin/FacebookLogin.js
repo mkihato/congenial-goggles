97fc2783d41ad4ffa36ec41d1bde40f8587d3f31
mkihato <mlamaar9@gmail.com>
2024-07-11 22:07:22 +0300

    thur

--- client/src/components/fbLogin/FacebookLogin.js
+++ client/src/components/fbLogin/FacebookLogin.js
@@ -5,28 +5,29 @@ const FacebookLogin = ({ onLogin }) => {
   useEffect(() => {
     // Initialize the Facebook SDK
     window.fbAsyncInit = function () {
-      // window.FB.init({
-      //   appId: '7767892629969878',
-      //   cookie: true,
-      //   xfbml: true,
-      //   version: 'v20.0'
-      // });
+      window.FB.init({
+        appId: '7767892629969878',
+        cookie: true,
+        xfbml: true,
+        version: 'v20.0'
+      });
 
-      // window.FB.AppEvents.logPageView();
+      window.FB.AppEvents.logPageView();
     };
 
     // Load the Facebook SDK script
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, 'script', 'facebook-jssdk');
+    // (function (d, s, id) {
+    //   var js,
+    //     fjs = d.getElementsByTagName(s)[0];
+    //   if (d.getElementById(id)) {
+    //     return;
+    //   }
+    //   js = d.createElement(s);
+    //   js.id = id;
+    //   js.src = 'https://connect.facebook.net/en_US/sdk.js#version=v20.0';
+    //   fjs.parentNode.insertBefore(js, fjs);
+    // })(document, 'script', 'facebook-jssdk');
+    
   }, []);
 
   const handleFBLogin = () => {
