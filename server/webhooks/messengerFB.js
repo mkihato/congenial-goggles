e9a83cfabbc5741f591e3b4d931d57a137c2d05c
mkihato <mlamaar9@gmail.com>
2024-07-21 21:26:44 +0300

    sun

--- server/webhooks/messengerFB.js
+++ server/webhooks/messengerFB.js
@@ -35,14 +35,18 @@ const receiveMessage= async (req, res) => {
   let body= req.body;
   console.log(body)
 
-  
-  if(body.object === 'page'){
+  if (body.object === 'page') {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-
       // Gets the body of the webhook event
       let webhookEvent = entry.messaging[0];
       console.log(webhookEvent);
+
+      // Check if sender is defined
+      if (!webhookEvent.sender || !webhookEvent.sender.id) {
+        console.error('Webhook event does not contain sender.id');
+        return res.status(400).send('Webhook event missing sender.id');
+      }
 
       // Get the sender PSID
       let senderPsid = webhookEvent.sender.id;
@@ -59,11 +63,39 @@ const receiveMessage= async (req, res) => {
 
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
-
-  }else{
+  } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+  
+  // if(body.object === 'page'){
+  //   // Iterates over each entry - there may be multiple if batched
+  //   body.entry.forEach(function(entry) {
+
+  //     // Gets the body of the webhook event
+  //     let webhookEvent = entry.messaging[0];
+  //     console.log(webhookEvent);
+
+  //     // Get the sender PSID
+  //     let senderPsid = webhookEvent.sender.id;
+  //     console.log('Sender PSID: ' + senderPsid);
+
+  //     // Check if the event is a message or postback and
+  //     // pass the event to the appropriate handler function
+  //     if (webhookEvent.message) {
+  //       handleMessage(senderPsid, webhookEvent.message);
+  //     } else if (webhookEvent.postback) {
+  //       handlePostback(senderPsid, webhookEvent.postback);
+  //     }
+  //   });
+
+  //   // Returns a '200 OK' response to all requests
+  //   res.status(200).send('EVENT_RECEIVED');
+
+  // }else{
+  //   // Returns a '404 Not Found' if event is not from a page subscription
+  //   res.sendStatus(404);
+  // }
   
 };
 
